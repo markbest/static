703416426e28ac6b95c6c877e037b4a6270cc61c
mafengtao <mafengtao@yfcloud.com>
2017-08-31 15:33:22 +0800

    代码优化

--- main.go
+++ main.go
@@ -33,19 +33,16 @@ func main() {
 	go grab.GenerateStaticCategory(category, cat)
 
 	//抓取文章数据
-	var total_page, i int64
 	articles := grab.GetArticles(article_api)
-	total_page = articles.Total/articles.Per_page + 1
+	total_page := articles.Total/articles.Per_page + 1
 	art := make(chan int64, total_page)
-	for i = 1; i <= total_page; i++ {
+	for i := 1; i <= total_page; i++ {
 		go processArticle(article_api+"?page="+strconv.FormatInt(i, 10), art)
 	}
 
 	//从channel中获取
 	<-cat
-	for i = 1; i <= total_page; i++ {
+	for i := 1; i <= total_page; i++ {
 		<-art
 	}
-	close(cat)
-	close(art)
 }
